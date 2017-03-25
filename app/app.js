daaa14e19bd6a504e27a82d2fb54056430a3c8cb
Xiaolin Song <xiaolin.song5@gmail.com>
2017-03-25 19:43:07 +0100

    Use ui-router

--- app/app.js
+++ app/app.js
@@ -1,13 +1,24 @@
 (function (){
-	var app = angular.module("contactform", ["ngRoute"]);
-	app.config(function($routeProvider) {
-		$routeProvider
-			.when("/onderwerp", {
-				templateUrl : "step1.html",
-				controller : "FormCtrl"
+
+	angular
+		.module('contactform', ['ngAnimate','ui.router','ngSanitize'])
+		.config(appRouting);
+
+	appRouting.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+	function appRouting ($stateProvider) {
+		$stateProvider
+			.state('step1', {
+				url: '/onderwerp',
+				templateUrl: 'step1.html',
+				controller: 'FormCtrl'
+			})
+			.state('step2', {
+				url: '/uw-gegevens',
+				templateUrl: 'step2.html',
 			});
-	});
-	
+
+	}
 
 }());
 
