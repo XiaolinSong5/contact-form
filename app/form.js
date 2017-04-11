6023820dfce0f4cbb8e66eb3b67d2353a91be9af
Xiaolin Song <xsong@anwb.nl>
2017-04-11 08:56:04 +0200

    Navigation between steps

--- app/form.js
+++ app/form.js
@@ -1,39 +1,18 @@
 (function () {
     angular
         .module('contactform')
-        .controller('FormCtrl', function($scope) {
-            $scope.firstName= "Xiaolin";
-            $scope.lastName= "Song";
+        .controller('FormCtrl', formCtrl);
+    formCtrl.$inject = ['$scope', '$state'];
 
-            // function to change ui state to next page
-            $scope.nextStep = function (location) {
-               
-                // check if valid to eather block the form if validation fails or let is go trough
-                $state.go(location); // go to next step
-               
-            };
-
-            $scope.submitForm = function () {
-                // check if valid to eather block the form if validation fails or let is go trough
-                if ($scope.contactForm.$valid) {
-                    $scope.formLogic.submittingData = true;
-                    var postData = cleanFormPost($scope.formFields);
-                    webService.submitForm(postData).success(function (data) {
-                        data.status === 200 ? _submitSucces() : _submitFail();
-                    }).error(function (data, status) {
-                        if (status === 400) {
-                            $scope.formLogic.submittingData = false;
-                            $scope.formLogic.backendFieldErrors = errorHandler.getErrorMessages(data);
-                            $scope.applyBackendValidation(data.fieldErrors);
-                        }
-                        else {
-                            _submitFail();
-                        }
-                    });
-                }
-            };
+    function formCtrl($scope, $state) {
+        // function to change ui state to next page
+        $scope.nextStep = function (location) {
 
 
-        });
+            // check if valid to eather block the form if validation fails or let is go trough
+       $state.go(location);
+
+        };
+    }
   
 }());
