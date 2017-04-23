6add212763b80ac909d8c4c4d875e92e8007066a
Xiaolin Song <xsong@anwb.nl>
2017-04-23 12:48:44 +0200

    CheckIrish

--- app/form.js
+++ app/form.js
@@ -10,7 +10,12 @@
 
 
             // check if valid to eather block the form if validation fails or let is go trough
-       $state.go(location);
+            $state.go(location);
+
+        };
+
+        $scope.checkIris = function () {
+            window.alert("checkIrisCalled");
 
         };
     }
